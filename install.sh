39b27c8714fba6e9576cda06152537bbdef39f00
Prophet <finn@famschneider.info>
2022-05-25 18:54:33 +0200

    Improve build script to include dependencies

--- install.sh
+++ install.sh
@@ -2,6 +2,22 @@
 
 set -euo pipefail
 
+
+if [ -z $(opam switch list -s | grep '^4.14.0$') ]; then
+    echo "Creating opam switch for 4.14.0"
+    opam switch create 4.14.0
+fi
+
+eval $(opam env --switch=4.14.0 --set-switch)
+
+echo "Installing dune"
+opam install dune
+
+echo "Installing menhir"
+opam install menhir
+echo "Installing other dependencies"
+opam install . --deps-only
+
 echo "Building project with dune"
 dune build
 dune install

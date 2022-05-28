baf0e844e7e49797ddc886d55dcfa7a4c93bb1c3
Prophet <finn@famschneider.info>
2022-05-28 22:03:08 +0200

    Add promise interface file

---
+++ src/promise.mli
@@ -0,0 +1,12 @@
+
+type 'a result = Finished of 'a
+               | Failed of exn
+               | Pending
+
+type 'a t
+
+val create : (unit -> 'a) -> 'a t 
+
+val peek : 'a t -> 'a result
+
+val await : 'a t -> 'a
